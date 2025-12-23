7cc662b5f3b7ae0cf36254373b97f5ecdf892d9c
snahangshu mallik <mallik00bis@gmail.com>
2025-12-23 13:04:18 +0530

    hhgh

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ from admin_routes import router as admin_router
 app = FastAPI(title="Door2Fy Associate Engineer Backend")
 
 # CORS for your React/Next/other frontend
-origins = [
-    "http://localhost:8080",
-    "https://partner.door2fy.in/",
-]
+# origins = [
+#     "http://localhost:8080",
+#     "https://partner.door2fy.in/",
+# ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["https://partner.door2fy.in"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
